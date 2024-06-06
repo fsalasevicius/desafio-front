6839967fa83b1fae4633602c1e560926d88b2831
Flavio Gastón Salasevicius <flaviosalasevicius@hotmail.com>
2024-06-06 19:03:40 -0300

    siguiendo con form predicciones

--- src/app/educal/games/copa-america/copa-america.component.ts
+++ src/app/educal/games/copa-america/copa-america.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Match, Team } from 'src/app/interface/copa-america.interface';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Match } from 'src/app/interface/copa-america.interface';
 import { CopaAmericaService } from 'src/app/services/copa-america.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-copa-america',
@@ -14,44 +15,52 @@ export class CopaAmericaComponent implements OnInit {
   loading = true;
   grupos: { nombre: string, partidos: Match[] }[] = [];
 
-  constructor(private fb: FormBuilder, private _copaAmericaService: CopaAmericaService) {}
+  constructor(private fb: FormBuilder, private _copaAmericaService: CopaAmericaService, private _messageService: MessageService) {
+    this.predictionForm = this.fb.group({
+      predictions: this.fb.array([])
+    });
+  }
 
   ngOnInit(): void {
-    this.predictionForm = this.fb.group({
-      predictions: this.fb.array([]) // Debe ser un FormArray vacío al principio
-    });
-
-    this._copaAmericaService.match_list().subscribe({
-      next: (response: any) => {
-        // Verificar si response.data es un array
-        if (Array.isArray(response.data)) {
-          this.matches = response.data.map((matchData: any) => {
-            return {
-              _id: matchData._id,
-              nmatch: matchData.nmatch,
-              teamA: matchData.teamA ? matchData.teamA : null,
-              teamB: matchData.teamB ? matchData.teamB : null,
-              date: matchData.date,
-              stadium: matchData.stadium ? matchData.stadium : null,
-              ticket: matchData.ticket,
-              fase: matchData.fase,
-              group: matchData.group,
-              createdAt: matchData.createdAt
-            };
-          });
+    this._copaAmericaService.match_list().subscribe(
+      (response: any) => {
+        this.matches = response.data;
+        if (Array.isArray(this.matches)) {
           this.groupMatches();
           this.addMatchControls();
           this.loading = false;
         } else {
-          console.error('La respuesta de la API no es un array:', response.data);
+          console.error('La respuesta de la API no es un array:', this.matches);
           this.loading = false;
         }
       },
-      error: (error) => {
+      error => {
         console.error('Error al obtener los partidos:', error);
         this.loading = false;
       }
+    );
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.grupos.forEach((grupo, gIndex) => {
+      const groupFormArray = this.fb.array([]);
+      grupo.partidos.forEach((partido, pIndex) => {
+        const partidoFormGroup = this.fb.group({
+          predictedScore: this.fb.group({
+            teamA: ['', Validators.required],
+            teamB: ['', Validators.required]
+          }),
+          predictedWinner: [''],
+          predictedGoalDifference: ['']
+        });
+        groupFormArray.push(partidoFormGroup);
+      });
+      (this.predictionForm.get('predictions') as FormArray).push(groupFormArray);
     });
+  
+    // Marcar el formulario como 'touched' después de inicializarlo
+    this.predictionForm.markAllAsTouched();
   }
 
   private addMatchControls(): void {
@@ -61,20 +70,18 @@ export class CopaAmericaComponent implements OnInit {
     });
   }
 
-  private createMatchGroup(match: any): FormGroup {
+  private createMatchGroup(match: Match): FormGroup {
     return this.fb.group({
       matchId: [match._id, Validators.required],
-      predictedWinner: ['', Validators.required],
       predictedScore: this.fb.group({
         teamA: ['', Validators.required],
         teamB: ['', Validators.required]
       }),
-      predictedGoalDifference: ['', Validators.required]
     });
   }
 
   groupMatches(): void {
-    const groupedMatches: { [key: string]: any[] } = {};
+    const groupedMatches: { [key: string]: Match[] } = {};
     this.matches.forEach(match => {
       if (!groupedMatches[match.group]) {
         groupedMatches[match.group] = [];
@@ -82,19 +89,50 @@ export class CopaAmericaComponent implements OnInit {
       groupedMatches[match.group].push(match);
     });
 
-    this.grupos = Object.keys(groupedMatches).map(grupo => ({
+    this.grupos = Object.keys(groupedMatches).sort().map(grupo => ({
       nombre: `Grupo ${grupo}`,
       partidos: groupedMatches[grupo]
     }));
   }
 
+  getInvalidControls(form: FormGroup | FormArray): string[] {
+    const invalidControls: string[] = [];
+    const controls = form.controls as { [key: string]: AbstractControl };
+    for (const name in controls) {
+      if (controls[name] instanceof FormGroup || controls[name] instanceof FormArray) {
+        const nestedInvalidControls = this.getInvalidControls(controls[name] as FormGroup | FormArray);
+        invalidControls.push(...nestedInvalidControls.map(controlName => `${name}.${controlName}`));
+      } else if (controls[name].invalid) {
+        invalidControls.push(name);
+      }
+    }
+    return invalidControls;
+  }
+  
   onSubmit(): void {
-    // Aquí puedes enviar los datos del formulario al servidor
-    console.log(this.predictionForm.value);
+    if (this.predictionForm.invalid) {
+      this.predictionForm.markAllAsTouched();
+      const invalidControls = this.getInvalidControls(this.predictionForm);
+      console.log('Campos inválidos:', invalidControls);
+      this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Faltan realizar predicciones.' });
+      return;
+    } else {
+      this._messageService.add({ severity: 'success', summary: 'Proceso Exitoso!', detail: 'Guardando Predicciones...' });
+    }
   }
 
   get predictions(): FormArray {
     return this.predictionForm.get('predictions') as FormArray;
+  }
+
+  getIconClass(teamA: number, teamB: number): string {
+    if (teamA > teamB) {
+      return 'fas fa-check-circle'; // Icono de check si teamA tiene más goles
+    } else if (teamA < teamB) {
+      return 'fas fa-times-circle'; // Icono de times si teamB tiene más goles
+    } else {
+      return 'fas fa-equals'; // Icono de igual si los goles son iguales
+    }
   }
 
   getFormGroupIndex(gIndex: number, pIndex: number): number {
