05b4879817dc12f23a563c0d0825597d54f1ff41
Flavio Gastón Salasevicius <flaviosalasevicius@hotmail.com>
2024-06-16 20:54:55 -0300

    cambiando server

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,4 +1,4 @@
 export var GLOBAL = {
-   url : 'http://localhost:4201/api/',
-   //url : 'https://back-xi-ten.vercel.app/api/',
+   //url : 'http://localhost:4201/api/',
+   url : 'https://back-xi-ten.vercel.app/api/',
 }
