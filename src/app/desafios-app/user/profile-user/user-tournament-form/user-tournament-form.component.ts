aee1caf5a22c37d856283d6256f101ef3d320603
Flavio Gastón Salasevicius <flaviosalasevicius@hotmail.com>
2024-06-17 13:45:52 -0300

    agregando detalle de torneos en componente de usuarios

--- src/app/desafios-app/user/profile-user/user-tournament-form/user-tournament-form.component.ts
+++ src/app/desafios-app/user/profile-user/user-tournament-form/user-tournament-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CopaAmericaService } from 'src/app/services/copa-america.service';
 
 @Component({
   selector: 'app-user-tournament-form',
@@ -6,10 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-tournament-form.component.scss']
 })
 export class UserTournamentFormComponent implements OnInit {
+  user: any = undefined;
+  loading: boolean = true;
+  public token = localStorage.getItem('authToken');
+  invitations: any[] = [];
+  public tournamentName = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _copaAmericaService: CopaAmericaService
+  ) {
+    if (this.token) {
+      let obj_lc: any = localStorage.getItem('userData');
+      this.user = JSON.parse(obj_lc);
+      console.log('User data from localStorage:', this.user);
+    }
   }
 
+  loadInvitations(): void {
+    this.loading = true;
+    if (this.user && this.user.email) {
+      const email = this.user.email;
+      this._copaAmericaService.getUserInvitations({ email }, this.token).subscribe(
+        (response) => {
+          this.invitations = response
+          console.log(this.invitations)
+          this.loading = false;
+        },
+        (error) => {
+          console.error('Error al obtener las invitaciones:', error);
+          this.loading = false;
+        }
+      );
+    } else {
+      this.loading = false;
+      console.log('No user or email found');
+    }
+  }
+
+  joinTournament(tournament: any): void {
+    this._copaAmericaService.joinTournament(
+      { tournamentName: tournament.tournamentName, userEmail: this.user.email },
+      this.token
+    ).subscribe(
+      (response) => {
+        this.loadInvitations(); 
+      },
+      (error) => {
+        console.error('Error al unirse al torneo:', error);
+      }
+    );
+}
+
+isPendingInvitation(tournament: any): boolean {
+  return tournament.invitations.some((inv: any) => inv.email === this.user.email && inv.status === 'pending');
+}
+
+  ngOnInit(): void {
+    console.log('User:', this.user);
+    this.loadInvitations();
+    console.log('Invitations after loadInvitations:', this.invitations);
+  }
 }
