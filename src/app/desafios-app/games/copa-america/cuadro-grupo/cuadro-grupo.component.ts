ec1e56e9931ed7bbe0532ff64e5d94e0a1c64af4
Flavio Gastón Salasevicius <flaviosalasevicius@hotmail.com>
2024-07-01 10:17:11 -0300

    cambios en grupos y listado de partidos

--- src/app/desafios-app/games/copa-america/cuadro-grupo/cuadro-grupo.component.ts
+++ src/app/desafios-app/games/copa-america/cuadro-grupo/cuadro-grupo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CopaAmericaService } from 'src/app/services/copa-america.service';
 import { formatDate } from '@angular/common';
+
 export interface Partido {
   grupo: string;
   fechaHora: string;
@@ -24,63 +25,76 @@ export interface Partido {
 export class CuadroGrupoComponent implements OnInit {
   loading = true;
   partidos: any = [];
+  filteredPartidos: any = [];
+  filtroActivo: string = 'all';
   formatFecha(fecha: Date): string {
     const formattedDate = formatDate(fecha, 'EEE, dd/MM/yyyy', 'es-AR');
     return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
   }
-  constructor(
-    private _copaAmericaService: CopaAmericaService,) { }
+  constructor(private _copaAmericaService: CopaAmericaService) { }
 
-    ngOnInit(): void {
-      this.loading = true;
-      this._copaAmericaService.match_list().subscribe(
-        (response: any) => {
-          this.partidos = response.data;
-          this.partidos.sort((a: { date: string; group: string; }, b: { date: string; group: any; }) => {
-            // Convertir las fechas a formato comparable (ej. dd/MM/yyyy hh:mm -> yyyyMMddhhmm)
-            const dateA = this.formatDate(a.date);
-            const dateB = this.formatDate(b.date);
-    
-            // Comparar las fechas y luego los grupos si las fechas son iguales
-            if (dateA < dateB) return -1;
-            if (dateA > dateB) return 1;
-            return a.group.localeCompare(b.group);
-          });
-        },
-        (error) => {
-          console.error('Error al obtener los partidos:', error);
-          this.loading = false;
-        }
-      );
-    }
-    
-    // Función para convertir la fecha y hora a un formato comparable
-    formatDate(dateString: string): string {
-      const [dayMonthYear, time] = dateString.split(' ');
-      const [day, month, year] = dayMonthYear.split('/');
-      const [hours, minutes] = time.split(':');
-      return `${year}${month}${day}${hours}${minutes}`;
-    }
-
-    calcularTiempoRestante(fechaPartidoStr: string): string {
-      const fechaPartido = new Date(fechaPartidoStr);
-      const ahora = new Date();
-      let diferencia = fechaPartido.getTime() - ahora.getTime();
-  
-      if (diferencia < 0) {
-          diferencia = 0; // Si la diferencia es negativa, establecerla en 0
+  ngOnInit(): void {
+    this._copaAmericaService.match_list().subscribe(
+      (response: any) => {
+        this.partidos = response.data;
+        console.log('Partidos obtenidos:', this.partidos); // Debug: Ver los partidos obtenidos
+        this.partidos.sort((a: { date: string; group: string; }, b: { date: string; group: any; }) => {
+          const dateA = this.formatDate(a.date);
+          const dateB = this.formatDate(b.date);
+          if (dateA < dateB) return -1;
+          if (dateA > dateB) return 1;
+          return a.group.localeCompare(b.group);
+        });
+        this.filter('next'); // Aplicar filtro 'next' por defecto
+      },
+      (error) => {
+        console.error('Error al obtener los partidos:', error);
       }
-  
-      if (diferencia === 0) {
-          return ''; // Si la diferencia es cero, devolver una cadena vacía
-      }
-      
-      const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-      const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
-      const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
-      
-      return `${dias} d ${horas} h ${minutos} m ${segundos} s`;
+    );
   }
 
+  formatDate(dateString: string): string {
+    if (!dateString) {
+      return '';
+    }
+
+    const [dayMonthYear, time] = dateString.split(' ');
+    const [day, month, year] = dayMonthYear.split('/');
+    const [hours, minutes] = time.split(':');
+    // Formato deseado: yyyyMMddhhmm
+    return `${year}${month}${day}${hours}${minutes}`;
+  }
+
+  calcularTiempoRestante(fechaPartidoStr: string): string {
+    if (!fechaPartidoStr) {
+      return '';
+    }
+
+    const fechaPartido = new Date(fechaPartidoStr);
+    const ahora = new Date();
+    let diferencia = fechaPartido.getTime() - ahora.getTime();
+
+    if (diferencia < 0) {
+      diferencia = 0;
+    }
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+    return `${dias} d ${horas} h ${minutos} m ${segundos} s`;
+  }
+
+  filter(type: string) {
+    this.filtroActivo = type;
+    if (type === 'next') {
+      this.filteredPartidos = this.partidos.filter((partido: any) => partido.state !== 'Finalizado');
+    } else if (type === 'previous') {
+      this.filteredPartidos = this.partidos.filter((partido: any) => partido.state === 'Finalizado');
+    } else if (type === 'all') {
+      this.filteredPartidos = this.partidos;
+    }
+    console.log('Filtered matches:', this.filteredPartidos);
+  }
 }
--- src/app/desafios-app/games/copa-america/cuadro-simulacion/cuadro-simulacion.component.ts
+++ src/app/desafios-app/games/copa-america/cuadro-simulacion/cuadro-simulacion.component.ts
@@ -4,37 +4,46 @@ import { CopaAmericaService } from 'src/app/services/copa-america.service';
 @Component({
   selector: 'app-tabla-puntos',
   templateUrl: './cuadro-simulacion.component.html',
-  styleUrls: ['./cuadro-simulacion.component.scss']
+  styleUrls: ['./cuadro-simulacion.component.scss'],
 })
-export class CuadroSimulacionComponent implements OnInit {   public groupStats: { [key: string]: { [teamId: string]: any } } = {};
-public loading = true;
-public groupedStats: { group: string, teams: { teamId: string, stats: any }[] }[] = [];
+export class CuadroSimulacionComponent implements OnInit {
+  public groupStats: { [key: string]: { [teamId: string]: any } } = {};
+  public loading = true;
+  public groupedStats: {
+    group: string;
+    teams: { teamId: string; stats: any }[];
+  }[] = [];
 
-constructor(private _copaAmericaService: CopaAmericaService) {}
-ngOnInit(): void {
-  this._copaAmericaService.table_resultados().subscribe(
-    response => {
-      this.groupStats = response.groupStats;
-      this.loading = false;
-      this.groupedStats = this.groupByGroup();
-    },
-    error => {
-      console.error(error);
-      this.loading = false;
-    }
-  );
-}
+  constructor(private _copaAmericaService: CopaAmericaService) {}
+  ngOnInit(): void {
+    this._copaAmericaService.table_resultados().subscribe(
+      (response) => {
+        this.groupStats = response.groupStats;
+        this.loading = false;
+        this.groupedStats = this.groupByGroup();
+      },
+      (error) => {
+        console.error(error);
+        this.loading = false;
+      }
+    );
+  }
 
-getGroupNames() {
-  return Object.keys(this.groupStats);
-}
+  getGroupNames() {
+    return Object.keys(this.groupStats);
+  }
 
-groupByGroup() {
-  return Object.keys(this.groupStats)
-    .sort() // Ordenar las claves de los grupos por orden alfabético
-    .map(group => ({
-      group,
-      teams: Object.entries(this.groupStats[group]).map(([teamId, stats]) => ({ teamId, stats }))
-    }));
-}
+  groupByGroup() {
+    const validGroups = ['A', 'B', 'C', 'D']; // Definir los grupos válidos
+
+    return Object.keys(this.groupStats)
+      .filter((group) => validGroups.includes(group)) // Filtrar los grupos válidos
+      .sort() // Ordenar las claves de los grupos por orden alfabético
+      .map((group) => ({
+        group,
+        teams: Object.entries(this.groupStats[group]).map(
+          ([teamId, stats]) => ({ teamId, stats })
+        ),
+      }));
+  }
 }
