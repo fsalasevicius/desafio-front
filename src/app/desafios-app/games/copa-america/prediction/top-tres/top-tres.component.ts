cbcc64cbcf4e5e11003849c2e7ac9fc5509b21e9
Flavio Gastón Salasevicius <flaviosalasevicius@hotmail.com>
2024-07-03 17:26:23 -0300

    agregando back

--- src/app/desafios-app/games/copa-america/prediction/top-tres/top-tres.component.ts
+++ src/app/desafios-app/games/copa-america/prediction/top-tres/top-tres.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 import { Team } from 'src/app/interface/copa-america.interface';
 import { CopaAmericaService } from 'src/app/services/copa-america.service';
-
 @Component({
   selector: 'app-top-tres',
   templateUrl: './top-tres.component.html',
-  styleUrls: ['./top-tres.component.scss']
+  styleUrls: ['./top-tres.component.scss'],
 })
 export class TopTresComponent implements OnInit {
   loading = true;
@@ -14,11 +14,17 @@ export class TopTresComponent implements OnInit {
   user: any = undefined;
   public token = localStorage.getItem('authToken');
   positionsForm: FormGroup;
-  existingPredictions: any = null; // Variable para almacenar predicciones existentes del usuario
+  existingPredictions: any = null;
+  days: number | undefined;
+  hours!: number;
+  minutes!: number;
+  seconds!: number;
+  showCountdown: boolean = true;
 
   constructor(
     private _fb: FormBuilder,
-    private _copaService: CopaAmericaService,
+    private _messageService: MessageService,
+    private _copaService: CopaAmericaService
   ) {
     if (this.token) {
       let obj_lc: any = localStorage.getItem('userData');
@@ -33,12 +39,12 @@ export class TopTresComponent implements OnInit {
 
   ngOnInit(): void {
     this.loading = true;
-  
+    this.startCountdown();
     // Verificar que this.user._id esté definido
-  
+
     // Obtener predicciones existentes del usuario
     this._copaService.view_prediction_top(this.user, this.token).subscribe(
-      response => {
+      (response) => {
         if (response && response.data && response.data.length > 0) {
           this.existingPredictions = response.data[0]; // Asignar la primera predicción (asumiendo que el usuario solo tiene una)
           this.populateFormWithPredictions(); // Llenar el formulario con las predicciones existentes
@@ -47,22 +53,28 @@ export class TopTresComponent implements OnInit {
         }
         this.loading = false;
       },
-      error => {
+      (error) => {
         console.error('Error al obtener las predicciones del usuario', error);
         this.loading = false;
       }
     );
-  
+
     // Obtener equipos del torneo
     this._copaService.getTeamTournament().subscribe(
-      teams => {
+      (teams) => {
         this.teams = teams.data;
         if (this.existingPredictions) {
           // Obtener los nombres de los equipos por ID
-          const firstPlaceName = this.getTeamNameById(this.existingPredictions.firstPlace);
-          const secondPlaceName = this.getTeamNameById(this.existingPredictions.secondPlace);
-          const thirdPlaceName = this.getTeamNameById(this.existingPredictions.thirdPlace);
-      
+          const firstPlaceName = this.getTeamNameById(
+            this.existingPredictions.firstPlace
+          );
+          const secondPlaceName = this.getTeamNameById(
+            this.existingPredictions.secondPlace
+          );
+          const thirdPlaceName = this.getTeamNameById(
+            this.existingPredictions.thirdPlace
+          );
+
           // Asignar los valores al formulario
           this.positionsForm.patchValue({
             firstPlace: firstPlaceName,
@@ -71,20 +83,26 @@ export class TopTresComponent implements OnInit {
           });
         }
       },
-      error => {
+      (error) => {
         console.error('Error al obtener los equipos del torneo', error);
         this.loading = false;
       }
     );
   }
-  
+
   populateFormWithPredictions(): void {
     if (this.existingPredictions) {
       // Obtener los nombres de los equipos por ID
-      const firstPlaceName = this.getTeamNameById(this.existingPredictions.firstPlace);
-      const secondPlaceName = this.getTeamNameById(this.existingPredictions.secondPlace);
-      const thirdPlaceName = this.getTeamNameById(this.existingPredictions.thirdPlace);
-  
+      const firstPlaceName = this.getTeamNameById(
+        this.existingPredictions.firstPlace
+      );
+      const secondPlaceName = this.getTeamNameById(
+        this.existingPredictions.secondPlace
+      );
+      const thirdPlaceName = this.getTeamNameById(
+        this.existingPredictions.thirdPlace
+      );
+
       // Asignar los valores al formulario
       this.positionsForm.patchValue({
         firstPlace: firstPlaceName,
@@ -94,26 +112,29 @@ export class TopTresComponent implements OnInit {
     }
   }
   getTeamNameById(id: string): string {
-    const team = this.teams.find(t => t._id.toString() === id);
+    const team = this.teams.find((t) => t._id.toString() === id);
     return team ? team.name : '';
   }
 
-  validateSelection(place: 'firstPlace' | 'secondPlace' | 'thirdPlace', event: Event) {
+  validateSelection(
+    place: 'firstPlace' | 'secondPlace' | 'thirdPlace',
+    event: Event
+  ) {
     const inputElement = event.target as HTMLInputElement;
     const selectedValue = inputElement.value.trim();
 
     // Verificar si el valor seleccionado está en la lista de equipos
-    const isValid = this.teams.some(team => team.name === selectedValue);
+    const isValid = this.teams.some((team) => team.name === selectedValue);
 
     // Si es válido, actualizar el valor en el formulario
     if (isValid) {
       this.positionsForm.patchValue({
-        [place]: selectedValue
+        [place]: selectedValue,
       });
     } else {
       // Si no es válido, limpiar el campo en el formulario
       this.positionsForm.patchValue({
-        [place]: ''
+        [place]: '',
       });
     }
   }
@@ -122,11 +143,10 @@ export class TopTresComponent implements OnInit {
     if (this.positionsForm.valid) {
       const predictions = { ...this.positionsForm.value };
   
-      ['firstPlace', 'secondPlace', 'thirdPlace'].forEach(place => {
+      // Convertir nombres de equipos en IDs
+      ['firstPlace', 'secondPlace', 'thirdPlace'].forEach((place) => {
         const teamName = predictions[place];
-  
-        const selectedTeam = this.teams.find(team => team.name === teamName);
-  
+        const selectedTeam = this.teams.find((team) => team.name === teamName);
         if (selectedTeam) {
           predictions[place] = selectedTeam._id;
         }
@@ -134,38 +154,97 @@ export class TopTresComponent implements OnInit {
   
       predictions.user = this.user._id;
   
-      // Verificar si ya existen predicciones
+      // Verificar fecha límite
+      const now = new Date();
+      const deadlineDate = new Date('2024-07-04T21:55:00-03:00'); // Fecha límite
+  
+      if (now > deadlineDate) {
+        // Mostrar mensaje de advertencia
+        this._messageService.add({
+          severity: 'warn',
+          summary: 'Fecha límite excedida',
+          detail: 'Ya no es posible actualizar las predicciones. La fecha límite ha sido superada.'
+        });
+        return; // Salir de la función si se ha superado la fecha límite
+      }
+  
+      // Guardar predicciones
       if (this.existingPredictions) {
-        // Si ya existen predicciones, actualizarlas
         this._copaService.register_prediction_top(predictions, this.token).subscribe(
-          response => {
-            // Aquí puedes manejar la respuesta del backend si es necesario
+          (response) => {
+            this._messageService.add({
+              severity: 'success',
+              summary: 'Operación Exitosa!',
+              detail: 'Tu predicción ha sido actualizada.'
+            });
           },
-          error => {
+          (error) => {
             console.error('Error al actualizar las predicciones del podio', error);
-            // Aquí puedes manejar errores y mostrar mensajes al usuario
+            this._messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'Error al actualizar las predicciones'
+            });
           }
         );
       } else {
-        // Si no existen predicciones, guardar nuevas
+        // Guardar nuevas predicciones
         this._copaService.register_prediction_top(predictions, this.token).subscribe(
-          response => {
-            // Aquí puedes manejar la respuesta del backend si es necesario
+          (response) => {
+            this._messageService.add({
+              severity: 'success',
+              summary: 'Operación Exitosa!',
+              detail: 'Tu predicción ha sido registrada.'
+            });
           },
-          error => {
+          (error) => {
             console.error('Error al guardar las predicciones del podio', error);
-            // Aquí puedes manejar errores y mostrar mensajes al usuario
+            this._messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'Tu predicción no se ha guardado, intenta nuevamente mas tarde.'
+            });
           }
         );
       }
     }
   }
+
   clearField(place: 'firstPlace' | 'secondPlace' | 'thirdPlace'): void {
     const formControl = this.positionsForm?.get(place);
     if (formControl) {
       formControl.reset(); // Resetear el campo específico del formulario
     } else {
-      console.error(`El formulario o el campo ${place} no están inicializados correctamente.`);
+      console.error(
+        `El formulario o el campo ${place} no están inicializados correctamente.`
+      );
     }
+  }
+
+ 
+  startCountdown(): void {
+    const countDownDate = new Date('Jul 04, 2024 21:55:00 GMT-0300').getTime();
+
+    const updateCountdown = () => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+
+      if (distance < 0) {
+        this.days = 0;
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+        this.showCountdown = false;
+        return;
+      }
+
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    };
+
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
   }
 }
